32b3f3352445ee319992b37e1cdb3d9f4df46006
StealWonders <louisduemler@mail.com>
2018-12-24 12:42:15 +0100

    changed ping command to use a cleaner approach and be more accurate

--- src/main/java/de/stealwonders/epicsuite/commands/PingCommand.java
+++ src/main/java/de/stealwonders/epicsuite/commands/PingCommand.java
@@ -15,15 +15,7 @@ public class PingCommand implements CommandExecutor {
 		if (sender instanceof Player) {
 
 			final Player player = (Player) sender;
-
-			try {
-				final Object entityPlayer = player.getClass().getMethod("getHandle").invoke(player);
-				final int ping = (int) entityPlayer.getClass().getField("ping").get(entityPlayer);
-				player.sendMessage("§dYour ping is " + ping + "ms");
-			} catch (final IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException | NoSuchFieldException exception) {
-				player.sendMessage("§cTheir was an error retrieving your ping. Please contact a server administrator.");
-				exception.printStackTrace();
-			}
+			player.sendMessage("§dYour ping is " + player.spigot().getPing() + "ms");
 
 		} else {
 			sender.sendMessage("You must be a player to execute this command.");
