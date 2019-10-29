6d01c663bf4d15dfaa8188547638a2a48cabfb10
StealWonders <louisduemler@mail.com>
2019-10-29 12:25:15 +0100

    Updated name highlighting to ignore links and use correct colors after a username.

--- src/main/java/de/stealwonders/epicsuite/chat/ChatHighlight.java
+++ src/main/java/de/stealwonders/epicsuite/chat/ChatHighlight.java
@@ -6,8 +6,12 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ChatHighlight implements Listener {
 
@@ -17,33 +21,83 @@ public class ChatHighlight implements Listener {
         this.luckPermsApi = luckPermsApi;
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onChat(final AsyncPlayerChatEvent event) {
         String message = event.getMessage();
+        final String findColorPattern = "(?<=§)\\w";
+
         for (final Player player : Bukkit.getOnlinePlayers()) {
             if (message.toLowerCase().contains(player.getName().toLowerCase())) {
 
-                final Group group = getGroup(event.getPlayer());
-                final String suffix = getSuffix(group);
-                final String colorString = suffix.substring(suffix.length() - 1);
-                final ChatColor color = ChatColor.getByChar(colorString);
+                final String[] parts = message.split(player.getName());
+                ChatColor lastColor = ChatColor.WHITE;
 
-                final String string = "§6§o" + player.getName() + color;
-                final String regex = "(?i)" + player.getName() + "(?-i)";
-                message = message.replaceAll(regex, string);
+                // If a player only says someones name without and text
+                if (parts.length == 0) {
+                    message = message.replaceAll(player.getName(), "§6§o" + player.getName());
+                }
+                
+                for (final String part : parts) {
+
+                    // Searches if the text part has a color code defined somewhere
+                    // If one is found it is set as the lastColor
+                    // If multiple are found the last one will be assigned
+                    final Pattern pattern = Pattern.compile(findColorPattern);
+                    final Matcher matcher = pattern.matcher(part);
+                    if (matcher.find()) {
+                        final String colorString = matcher.group(matcher.groupCount());
+                        lastColor = ChatColor.getByChar(colorString);
+                    }
+
+                    // Defines the name replacement Text
+                    final String coloredName = "§6§o" + player.getName() + lastColor;
+                    // Defines which username (if there are multiple in the message) should be replaced this time
+                    final String replacementRegex = "(?<=" + part + ")" + player.getName() + "(?-i)";
+                    // Defines the regex for getting text around a username to the nearest whitespace to identify if it's link
+                    final String linkRegex = "[^ ]+(?<=" + part + ")" + player.getName() + "[^ ]+";
+
+                    String potentialLinkString = null;
+
+                    final Pattern linkPattern = Pattern.compile(linkRegex);
+                    final Matcher linkMatcher = linkPattern.matcher(message);
+
+                    if (linkMatcher.find()) {
+                        potentialLinkString = linkMatcher.group(linkMatcher.groupCount());
+                    }
+
+                    // Only replace the username if no link pattern was found
+                    if (potentialLinkString == null) {
+                        message = message.replaceAll(replacementRegex, coloredName);
+                    } else {
+                        if (!isLink(potentialLinkString)) {
+                            message = message.replaceAll(replacementRegex, coloredName);
+                        }
+                    }
+                }
+
                 event.setMessage(message);
-
             }
         }
     }
 
+    private boolean isLink(final String string) {
+        final String regex = "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)";
+        final Pattern pattern = Pattern.compile(regex);
+        final Matcher matcher = pattern.matcher(string);
+        return matcher.find();
+    }
+
     private Group getGroup(final Player player) {
-        Track track = luckPermsApi.getTrack("default");
-        User user = luckPermsApi.getUser(player.getUniqueId());
-        for (String groupName : track.getGroups()) {
-            Group group = luckPermsApi.getGroup(groupName);
-            if (user.inheritsGroup(group)) {
-                return group;
+        final Track track = luckPermsApi.getTrack("default");
+        if (track != null) {
+            System.out.println(track);
+            final User user = luckPermsApi.getUser(player.getUniqueId());
+            System.out.println(user);
+            for (final String groupName : track.getGroups()) {
+                final Group group = luckPermsApi.getGroup(groupName);
+                if (user.inheritsGroup(group)) {
+                    return group;
+                }
             }
         }
         return null;
