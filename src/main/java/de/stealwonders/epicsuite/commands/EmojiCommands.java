5613b4053c21682e9b96a9ed70af56351c6fd38b
StealWonders <6114858+StealWonders@users.noreply.github.com>
2020-10-10 20:36:35 +0200

    Closes https://github.com/StealWonders/EpicSuite/issues/6

--- src/main/java/de/stealwonders/epicsuite/commands/EmojiCommands.java
+++ src/main/java/de/stealwonders/epicsuite/commands/EmojiCommands.java
@@ -3,10 +3,28 @@ package de.stealwonders.epicsuite.commands;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
+import co.aikar.commands.annotation.Default;
+import co.aikar.commands.annotation.Subcommand;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+@CommandAlias("emote")
 @CommandPermission("epicsuite.chat.emojis")
 public class EmojiCommands extends BaseCommand {
+
+    @Default
+    @Subcommand("list")
+    public void onCommand(final CommandSender commandSender) {
+        commandSender.sendMessage("");
+        commandSender.sendMessage(" §e/shrug      §b¯\\_(ツ)_/¯");
+        commandSender.sendMessage(" §e/tableflip  §b(╯°□°）╯︵ ┻━┻");
+        commandSender.sendMessage(" §e/unflip     §b┬─┬ ノ( ゜-゜ノ)");
+        commandSender.sendMessage(" §e/riot       §bヽ༼ຈل͜ຈ༽ﾉ RIOT ヽ༼ຈل͜ຈ༽ﾉ\"");
+        commandSender.sendMessage(" §e/lenny      §b( ͡° ͜ʖ ͡°)");
+        commandSender.sendMessage(" §e/sad        §b(ಥ﹏ಥ)");
+        commandSender.sendMessage(" §e/kawaii     §b(づ｡◕‿‿◕｡)づ");
+        commandSender.sendMessage("");
+    }
 
     @CommandAlias("shrug")
     public void onShrug(final Player player) {
